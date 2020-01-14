83fa75b906a76614df5ce21ca359e7f24a11d712
Jerry Gao <jerry@edapp.com>
2020-01-14 15:04:13 -0500

    create-react-app typescript and prettier

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+  stories: ['../src/**/*.stories.tsx'],
+  addons: [
+    {
+      name: '@storybook/preset-create-react-app',
+      options: {
+        tsDocgenLoaderOptions: {}
+      }
+    },
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true
+      }
+    }
+  ]
+};
