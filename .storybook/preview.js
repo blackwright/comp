fc11a2f0297fb9cd7dbaa4f7f28aa0a4c7351056
Jerry Gao <gao@blackwright.com>
2020-01-29 08:53:30 -0500

    add styled-components ThemeProvider in storybook decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,19 @@
-import { addParameters } from '@storybook/react';
+import React from 'react';
+import { addParameters, addDecorator } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
+import theme from 'theme';
+
+if (process.env.NODE_ENV !== 'test') {
+  addDecorator(storyFn => (
+    <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
+  ));
+}
 
 addParameters({
   options: {
+    // Stories are separated into categories at root level
     showRoots: true,
+    // Sort stories in alphabetical order
     storySort: (a, b) => {
       if (a[1].kind < b[1].kind) {
         return -1;
