4b4ecc835234ebe684bef2a2011acd2a8b1fe151
Jerry Gao <gao@blackwright.com>
2020-01-29 09:02:22 -0500

    move MatchMedia component into 'components' folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,4 @@ export { Button, Props as ButtonProps } from './inputs/button';
 export { Input, Props as InputProps } from './inputs/input';
 export { NumberInput, Props as NumberInputProps } from './inputs/number-input';
 export { TextArea, Props as TextAreaProps } from './inputs/text-area';
-export { MatchMedia, Props as MatchMediaProps } from '../match-media';
+export { MatchMedia, Props as MatchMediaProps } from './match-media';
