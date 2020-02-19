a56446ae8ab43b2e2bf6f78dfea9ce566fb4ccef
Jerry Gao <gao@blackwright.com>
2020-02-19 08:13:11 -0500

    add gray colors

--- src/theme/colors/raw/monochrome.ts
+++ src/theme/colors/raw/monochrome.ts
@@ -3,27 +3,36 @@ import { RGB } from '../types';
 const black: RGB = [0, 0, 0];
 
 // the higher the number, the darker the color
-const dark1: RGB = [56, 56, 56];
-const dark2: RGB = [35, 35, 35];
-const dark3: RGB = [27, 27, 27];
 const dark4: RGB = [11, 11, 11];
+const dark3: RGB = [27, 27, 27];
+const dark2: RGB = [35, 35, 35];
+const dark1: RGB = [56, 56, 56];
+
+const gray4: RGB = [120, 120, 120];
+const gray3: RGB = [132, 132, 132];
+const gray2: RGB = [146, 146, 146];
+const gray1: RGB = [160, 160, 160];
+
+const light4: RGB = [218, 218, 218];
+const light3: RGB = [236, 236, 236];
+const light2: RGB = [242, 242, 242];
+const light1: RGB = [248, 248, 248];
 
 const white: RGB = [255, 255, 255];
 
-const light1: RGB = [248, 248, 248];
-const light2: RGB = [242, 242, 242];
-const light3: RGB = [236, 236, 236];
-const light4: RGB = [218, 218, 218];
-
 export const monochrome = {
-  dark1,
-  dark2,
-  dark3,
-  dark4,
   black,
-  white,
-  light1,
-  light2,
+  dark4,
+  dark3,
+  dark2,
+  dark1,
+  gray4,
+  gray3,
+  gray2,
+  gray1,
+  light4,
   light3,
-  light4
+  light2,
+  light1,
+  white
 };
