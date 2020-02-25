47160a89d7a63f0c5d18aad393e92570ff713474
Jerry Gao <gao@blackwright.com>
2020-02-25 08:54:14 -0500

    consolidate Radio stories

--- src/components/inputs/radio/Radio.stories.tsx
+++ src/components/inputs/radio/Radio.stories.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
+import { RadioGroup } from './RadioGroup';
 import { Radio } from './Radio';
 
 export default {
   title: 'Inputs/Radio',
-  component: Radio
+  component: RadioGroup
 };
 
-export const Default = () => <Radio value={null}>Description</Radio>;
+export const Default = () => {
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <RadioGroup name="number" value={value} onChange={setValue}>
+      <Radio value={0} alignItems="center">
+        0
+      </Radio>
+      <Radio value={1} alignItems="center">
+        1
+      </Radio>
+      <Radio value={2} alignItems="center">
+        2
+      </Radio>
+    </RadioGroup>
+  );
+};
