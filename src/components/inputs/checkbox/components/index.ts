121f2ed94827281fd86395f225829c90113a4788
Jerry Gao <gao@blackwright.com>
2020-03-02 07:46:42 -0500

    update type exports from Checkbox

--- src/components/inputs/checkbox/components/index.ts
+++ src/components/inputs/checkbox/components/index.ts
@@ -1,7 +1,12 @@
-export { Label, Props as LabelProps } from './label';
+import { Props as _LabelProps } from './label';
+import { Props as _VisibleCheckboxProps } from './visible-checkbox';
+
+export { Label } from './label';
+export type LabelProps = _LabelProps;
+
 export { HiddenCheckbox } from './hidden-checkbox';
-export {
-  VisibleCheckbox,
-  Props as VisibleCheckboxProps
-} from './visible-checkbox';
+
+export { VisibleCheckbox } from './visible-checkbox';
+export type VisibleCheckboxProps = _VisibleCheckboxProps;
+
 export { Description } from './description';
