20072e7dc5af83421f071613d4493704d036d705
Jerry Gao <gao@blackwright.com>
2020-03-04 18:04:20 -0500

    fix Checkbox type exports

--- src/components/inputs/checkbox/index.ts
+++ src/components/inputs/checkbox/index.ts
@@ -1,3 +1,10 @@
+import {
+  LabelProps as _LabelProps,
+  VisibleCheckboxProps as _VisibleCheckboxProps
+} from './components';
+
 export { Checkbox } from './Checkbox';
-export { LabelProps, VisibleCheckboxProps } from './components';
 export * from './types';
+
+export type CheckboxLabelProps = _LabelProps;
+export type VisibleCheckboxProps = _VisibleCheckboxProps;
