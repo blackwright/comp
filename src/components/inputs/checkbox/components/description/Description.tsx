5e8fe903fdf3ba0698fba786e69c2b00644ed117
Jerry Gao <gao@blackwright.com>
2020-03-08 21:54:31 -0400

    move checkbox margin into Description component

--- src/components/inputs/checkbox/components/description/Description.tsx
+++ src/components/inputs/checkbox/components/description/Description.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Description = styled.div`
+  margin-left: ${({ theme: { sizing } }) => sizing.fn()}px;
   display: flex;
   align-items: flex-start;
   flex-flow: column nowrap;
