ef2a6d18f69bdfc97b6ceb213a47483170399c32
Jerry Gao <gao@blackwright.com>
2020-03-15 21:35:46 -0400

    add testId, default props to Badge

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -1,17 +1,22 @@
 import styled, { css } from 'styled-components';
 import { Props } from './types';
 
-export const Badge = styled.span<Props>(
-  ({ variant = 'neutral', theme: { sizing, font, colors } }) => {
-    const variantColors = colors.variants[variant] ?? colors.variants.neutral;
+export const Badge = styled.span.attrs<Props>(({ testId }) => ({
+  'data-testid': testId
+}))<Props>(({ variant = 'neutral', theme: { sizing, font, colors } }) => {
+  const variantColors = colors.variants[variant] ?? colors.variants.neutral;
 
-    return css`
-      padding: ${sizing.fn(0.25)}px ${sizing.fn(0.5)}px;
-      font-size: ${font.variants.feather.size};
-      color: ${variantColors.text.hex};
-      background: ${variantColors.background.passive.hex};
-      border-radius: ${sizing.borderRadius};
-      display: inline-flex;
-    `;
-  }
-);
+  return css`
+    padding: ${sizing.fn(0.25)}px ${sizing.fn(0.5)}px;
+    font-size: ${font.variants.feather.size};
+    color: ${variantColors.text.hex};
+    background: ${variantColors.background.passive.hex};
+    border-radius: ${sizing.borderRadius};
+    display: inline-flex;
+  `;
+});
+
+Badge.defaultProps = {
+  testId: 'badge',
+  variant: 'neutral'
+};
