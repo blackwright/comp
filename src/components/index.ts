a9c791b84c76f000bb46ec2befd6af55a515fafd
Jerry Gao <gao@blackwright.com>
2020-03-15 21:34:18 -0400

    update main barrel exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,17 +1,45 @@
 import { Props as _ButtonProps } from './inputs/button';
+import {
+  Props as _CheckboxProps,
+  VisibleCheckboxProps as _VisibleCheckboxProps,
+  CheckboxLabelProps as _CheckboxLabelProps
+} from './inputs/checkbox';
 import { Props as _InputProps } from './inputs/input';
 import { Props as _NumberInputProps } from './inputs/number-input';
+import {
+  RadioGroupProps as _RadioGroupProps,
+  RadioProps as _RadioProps
+} from './inputs/radio';
+import {
+  Props as _SelectProps,
+  Options as _SelectOptions
+} from './inputs/select';
 import { Props as _TextAreaProps } from './inputs/text-area';
-import { Props as _MatchMediaProps } from './match-media';
+import { Props as _ToggleProps } from './inputs/toggle';
+import {
+  Props as _MatchMediaProps,
+  Query as _MatchMediaQuery
+} from './match-media';
 import {
   Props as _TypographyProps,
   Variant as _TypographyVariant
 } from './typography';
 import { Props as _BlockProps } from './block';
+import { Props as _CardProps } from './card';
 import { Props as _DialogProps } from './dialog';
+import {
+  DropdownProps as _DropdownProps,
+  DropdownItemProps as _DropdownItemProps
+} from './dropdown';
+import { Props as _BadgeProps } from './badge';
 
 export { Button } from './inputs/button';
 export type ButtonProps = _ButtonProps;
+
+export { Checkbox } from './inputs/checkbox';
+export type CheckboxProps = _CheckboxProps;
+export type VisibleCheckboxProps = _VisibleCheckboxProps;
+export type CheckboxLabelProps = _CheckboxLabelProps;
 
 export { Input } from './inputs/input';
 export type InputProps = _InputProps;
@@ -19,11 +47,23 @@ export type InputProps = _InputProps;
 export { NumberInput } from './inputs/number-input';
 export type NumberInputProps = _NumberInputProps;
 
+export { RadioGroup, Radio } from './inputs/radio';
+export type RadioGroupProps = _RadioGroupProps;
+export type RadioProps = _RadioProps;
+
+export { Select } from './inputs/select';
+export type SelectProps<Option> = _SelectProps<Option>;
+export type SelectOptions<Option> = _SelectOptions<Option>;
+
 export { TextArea } from './inputs/text-area';
 export type TextAreaProps = _TextAreaProps;
 
+export { Toggle } from './inputs/toggle';
+export type ToggleProps = _ToggleProps;
+
 export { MatchMedia } from './match-media';
-export type MatchMediaProps = _MatchMediaProps;
+export type MatchMediaQuery = _MatchMediaQuery;
+export type MatchMediaProps<Q extends MatchMediaQuery> = _MatchMediaProps<Q>;
 
 export { Typography } from './typography';
 export type TypographyProps = _TypographyProps;
@@ -32,5 +72,15 @@ export type TypographyVariant = _TypographyVariant;
 export { Block } from './block';
 export type BlockProps = _BlockProps;
 
+export { Card } from './card';
+export type CardProps = _CardProps;
+
 export { Dialog } from './dialog';
 export type DialogProps = _DialogProps;
+
+export { Dropdown } from './dropdown';
+export type DropdownProps = _DropdownProps;
+export type DropdownItemProps = _DropdownItemProps;
+
+export { Badge } from './badge';
+export type BadgeProps = _BadgeProps;
