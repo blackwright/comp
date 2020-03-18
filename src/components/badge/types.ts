b9bea8ec10e16afebc3f71339f1238ccb85c1fa8
Jerry Gao <gao@blackwright.com>
2020-03-17 22:59:34 -0400

    remove unused Badge variant type

--- src/components/badge/types.ts
+++ src/components/badge/types.ts
@@ -1,38 +1,5 @@
 import { ComponentProps } from 'types';
-import { ColorValues, colors } from 'theme/colors';
-
-type VariantStyle = {
-  color: ColorValues;
-  background: ColorValues;
-};
-
-export const variantToColorsMapping: Record<
-  'positive' | 'negative' | 'neutral' | 'error' | 'warning',
-  VariantStyle
-> = {
-  positive: {
-    color: colors.green10,
-    background: colors.green1
-  },
-  negative: {
-    color: colors.red10,
-    background: colors.red1
-  },
-  neutral: {
-    color: colors.dark4,
-    background: colors.light3
-  },
-  error: {
-    color: colors.text.light,
-    background: colors.red8
-  },
-  warning: {
-    color: colors.yellow10,
-    background: colors.yellow2
-  }
-};
-
-export type VariantName = keyof typeof variantToColorsMapping;
+import { VariantName } from 'theme/colors';
 
 export type Props = {
   /**
